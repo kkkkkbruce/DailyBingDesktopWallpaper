2a02a2f2064b5d40b3d6fefb2f6e726921be79e2
kkkkkbruce <kkkkkbruce@users.noreply.github.com>
2020-07-03 10:05:45 -0500

    Add files via upload
    simple batch file for running script at startup in Windows

---
+++ paperchange.bat
@@ -0,0 +1,5 @@
+::paperchange.bat
+::add this .bat file to
+::C:\Users\current_user\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\
+::#to run at startup (change username and path accordingly)
+python C:\Users\current_user\Documents\bing_screen_wallpaper_windows.py
